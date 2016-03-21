9c12c29bf756af2b0059c144b7960d6ff3a462a8
Elton Silva <elton.chrls@gmail.com>
2016-03-21 17:23:07 -0300

    ok

--- components/site/app/assets/javascripts/site/application.js
+++ components/site/app/assets/javascripts/site/application.js
@@ -21,5 +21,6 @@
 //= require site/jquery.carouFredSel-6.2.1-packed
 //= require site/imagesloaded.pkgd.min
 //= require site/property-slider
+//= require site/bxslider
 //= require site/script
 //= require site/facebook
